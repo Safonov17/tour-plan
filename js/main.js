aaf536b46ba9d8dcf9580f7ea6618207e671106f
Ilya Safonov <safonovil17@yandex.ru>
2021-11-23 15:30:09 +0300

    fix: адаптив для слайдера + автопрокрутка

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
 
+  autoplay: {
+    delay: 3000,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: ".slider-button--next",
