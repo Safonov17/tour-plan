df5c766b9477a6556742624b6d42f000d56de915
Ilya Safonov <safonovil17@yandex.ru>
2021-11-29 22:24:56 +0300

    fix: поправил параллакс

--- js/main.js
+++ js/main.js
@@ -45,3 +45,18 @@ function init() {
 // Код для parallax эффекта
 
 $(".parallax-window").parallax({ imageSrc: "img/newsletter-bg.jpg" });
+
+// Мягкий скролл
+
+$(document).bind("mousewheel", function (e) {
+  var nt = $(document.body).scrollTop() - e.deltaY * e.deltaFactor * 100;
+  e.preventDefault();
+  e.stopPropagation();
+  $(document.body).stop().animate(
+    {
+      scrollTop: nt,
+    },
+    100,
+    "easeInOutCubic"
+  );
+});
