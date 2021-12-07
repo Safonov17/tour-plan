a86022a7c4cb5d43867cf8da1a4d35d752e693de
Ilya Safonov <safonovil17@yandex.ru>
2021-12-07 18:51:17 +0300

    create: modal window with media styles

--- js/main.js
+++ js/main.js
@@ -1,53 +1,74 @@
-const hotelSwiper = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  autoplay: {
-    delay: 3000,
-  },
-  // Кнопки навигации
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Кнопки навигации
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
-
-// Карта
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [7.838190124409071, 98.29879180041281],
-    zoom: 17,
-    controls: ["smallMapDefaultSet"],
+$(document).ready(function () {
+  const hotelSwiper = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    // Кнопки навигации
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
   });
-  var placemark = new ymaps.Placemark([7.838190124409071, 98.29879180041281], {
-    balloonContent: "Hilton Phuket Arcadia Resort & SPA",
-    // hintContent: "Гостиница",
+
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Кнопки навигации
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
   });
-  myMap.geoObjects.add(placemark);
-  placemark.balloon.open();
-}
 
-// Код для parallax эффекта
+  // Карта
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [7.838190124409071, 98.29879180041281],
+      zoom: 17,
+      controls: ["smallMapDefaultSet"],
+    });
+    var placemark = new ymaps.Placemark([7.838190124409071, 98.29879180041281], {
+      balloonContent: "Hilton Phuket Arcadia Resort & SPA",
+      // hintContent: "Гостиница",
+    });
+    myMap.geoObjects.add(placemark);
+    placemark.balloon.open();
+  }
 
-$(".parallax-window").parallax({ imageSrc: "img/newsletter-bg.jpg" });
+  // Код для parallax эффекта
 
-// меню
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".header__bottom").classList.toggle("header__bottom-visible");
+  $(".parallax-window").parallax({ imageSrc: "img/newsletter-bg.jpg" });
+
+  // меню
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".header__bottom").classList.toggle("header__bottom-visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay-visible");
+    modalDialog.addClass("modal__dialog-visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay-visible");
+    modalDialog.removeClass("modal__dialog-visible");
+  }
 });
